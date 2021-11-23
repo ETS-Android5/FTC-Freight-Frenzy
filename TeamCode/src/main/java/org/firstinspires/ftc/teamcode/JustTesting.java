67d6ad071b8632f76b383824a4a56c8181fe426c
ArnArora <arnavarora.ca@gmail.com>
2021-11-22 23:11:52 -0800

    Just Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JustTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JustTesting.java
@@ -3,5 +3,6 @@ package org.firstinspires.ftc.teamcode;
 public class JustTesting {
     public static void main(String[] args){
         System.out.println("Sup my dudes");
+        System.out.println();
     }
 }
