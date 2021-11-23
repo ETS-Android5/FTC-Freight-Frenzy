5eab304973c98a6ce516557765eee5343de6c4d8
siddhpatani <siddhrupal@gmail.com>
2021-11-22 23:14:50 -0800

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 public class Testing {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("sup gay");
     }
 
 }
